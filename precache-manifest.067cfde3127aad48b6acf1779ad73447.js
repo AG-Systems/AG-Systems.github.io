06461a1ba82a9a835a13f6466caf9ccfe07eea09
AG-Systems <maxchakhmatov@gmail.com>
2020-03-21 21:50:31 -0700

    Updates

---
+++ precache-manifest.067cfde3127aad48b6acf1779ad73447.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdaf555f7c3bffb5be61e954a273288",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87d30290e0b466d9ba2a",
+    "url": "/static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "94153d124a2f37205926",
+    "url": "/static/css/main.4e288dd3.chunk.css"
+  },
+  {
+    "revision": "87d30290e0b466d9ba2a",
+    "url": "/static/js/2.ca21885f.chunk.js"
+  },
+  {
+    "revision": "b311f1e546da055dadccce71e0551bcc",
+    "url": "/static/js/2.ca21885f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94153d124a2f37205926",
+    "url": "/static/js/main.cb76f9fe.chunk.js"
+  },
+  {
+    "revision": "717a3ad2364a97b18159",
+    "url": "/static/js/runtime-main.f72a2849.js"
+  },
+  {
+    "revision": "71e25f0738b326304fa697e4891cd76d",
+    "url": "/static/media/25048.71e25f07.jpg"
+  },
+  {
+    "revision": "80557e6ddca6673b7275451e2be6cf75",
+    "url": "/static/media/58341.80557e6d.jpg"
+  },
+  {
+    "revision": "d8e1c1e8b3bbe1f389a096793c2cd327",
+    "url": "/static/media/Wall-ESoundtrack.d8e1c1e8.jpg"
+  },
+  {
+    "revision": "43f0757683cec56f75b44b17edecbd8c",
+    "url": "/static/media/bg_red.43f07576.png"
+  },
+  {
+    "revision": "2c38ec0b395dd1225f404953a1f20e3c",
+    "url": "/static/media/eb018d403fa9633f426fded17f843b73.2c38ec0b.jpg"
+  },
+  {
+    "revision": "98a028c54f772fe4cb0f912444d61b3c",
+    "url": "/static/media/photo-1507041957456-9c397ce39c97.98a028c5.jpg"
+  },
+  {
+    "revision": "9ee477085092184ac2d7a8541cb68aea",
+    "url": "/static/media/photo-1533683083439-1a776a5653cb.9ee47708.jpg"
+  },
+  {
+    "revision": "9f390f733b676b8b1b7cdd086e6bb6fb",
+    "url": "/static/media/radial_2.9f390f73.png"
+  },
+  {
+    "revision": "f4b3edb85206539f69cd849c83019adc",
+    "url": "/static/media/shutterstock_674781376.0.f4b3edb8.jpg"
+  },
+  {
+    "revision": "e6ca02ca02e4b3960c964002a7205b43",
+    "url": "/static/media/thecoderschool.e6ca02ca.png"
+  }
+]);
