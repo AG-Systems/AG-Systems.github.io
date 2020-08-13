5d032d9538f9a38c8ef1c52c544ac4faa193bab7
AG-Systems <maxchakhmatov@gmail.com>
2020-08-13 00:06:00 -0700

    save some results

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -63,92 +63,102 @@ class Card extends React.Component {
     render() {
       const props = this.props;
       const data_list = props.data;
-      
-      let data_cards = data_list.map(function(exp, index)
+
+      if(data_list && data_list.length !== 0)
       {
-        let difference_months = "";
-        if(exp.date_ended == "Present")
-        {
-          difference_months = DATE_DIFF(exp.date_started, (new Date(new Date().getFullYear(),new Date().getMonth())).toString(), 'Month').output + 1;
-        } else {
-          difference_months = DATE_DIFF(exp.date_started, exp.date_ended, "Month").output + 1;
-        }
+            let data_cards = data_list.map(function(exp, index)
+            {
+              let difference_months = "";
+              if(exp.date_ended == "Present")
+              {
+                difference_months = DATE_DIFF(exp.date_started, (new Date(new Date().getFullYear(),new Date().getMonth())).toString(), 'Month').output + 1;
+              } else {
+                difference_months = DATE_DIFF(exp.date_started, exp.date_ended, "Month").output + 1;
+              }
 
-        let date_element = "";
-        if(difference_months >= 12)
-        {
-          let years = 0;
-          let months = 0;
-          while(difference_months >= 12)
-          {
-            years += 1;
-            difference_months = difference_months - 12;
-          }
-          months = difference_months;
-          if(months > 0)
-          {
-            date_element = <span className="card-date" style={{ color: props.dark_mode_props ? "rgba(226, 226, 226, 0.6)" : "rgba(0, 0, 0, 0.6)" }} > { exp.date_started } - { exp.date_ended } • { years } { years > 1 ? "Years" : "Year" } { difference_months } { difference_months > 1 ? "Months" : "Month" } </span>;
-          } else {
-            date_element = <span className="card-date" style={{ color: props.dark_mode_props ? "rgba(226, 226, 226, 0.6)" : "rgba(0, 0, 0, 0.6)" }} > { exp.date_started } - { exp.date_ended } • { years } { years > 1 ? "Years" : "Year" } </span>;
-          }
-        } else {
-          date_element = <span className="card-date" style={{ color: props.dark_mode_props ? "rgba(226, 226, 226, 0.6)" : "rgba(0, 0, 0, 0.6)" }} > { exp.date_started } - { exp.date_ended } • { difference_months } { difference_months > 1 ? "Months" : "Month" } </span>;
-        }
+              let date_element = "";
+              if(difference_months >= 12)
+              {
+                let years = 0;
+                let months = 0;
+                while(difference_months >= 12)
+                {
+                  years += 1;
+                  difference_months = difference_months - 12;
+                }
+                months = difference_months;
+                if(months > 0)
+                {
+                  date_element = <span className="card-date" style={{ color: props.dark_mode_props ? "rgba(226, 226, 226, 0.6)" : "rgba(0, 0, 0, 0.6)" }} > { exp.date_started } - { exp.date_ended } • { years } { years > 1 ? "Years" : "Year" } { difference_months } { difference_months > 1 ? "Months" : "Month" } </span>;
+                } else {
+                  date_element = <span className="card-date" style={{ color: props.dark_mode_props ? "rgba(226, 226, 226, 0.6)" : "rgba(0, 0, 0, 0.6)" }} > { exp.date_started } - { exp.date_ended } • { years } { years > 1 ? "Years" : "Year" } </span>;
+                }
+              } else {
+                date_element = <span className="card-date" style={{ color: props.dark_mode_props ? "rgba(226, 226, 226, 0.6)" : "rgba(0, 0, 0, 0.6)" }} > { exp.date_started } - { exp.date_ended } • { difference_months } { difference_months > 1 ? "Months" : "Month" } </span>;
+              }
 
-        let description_content = [];
-        description_content.push(<br key={ hash("desktop-" + exp.date_started + index + Math.floor(Math.random() * Math.floor(1000))) } />);
-        for (let value of data_list[index].description) {
-          description_content.push(<span className="dark-mode-card-desc" key={ hash("desktop-" + exp.date_ended + index + Math.floor(Math.random() * Math.floor(1000)))}> { value } </span>);
-          description_content.push(<br key={ hash("desktop-" + exp.date_ended + index + Math.floor(Math.random() * Math.floor(1000))) } />);
-        }
-        description_content.push(<br key={ hash("desktop-" + exp.date_ended + index + Math.floor(Math.random() * Math.floor(1000))) } />);
+              let description_content = [];
+              description_content.push(<br key={ hash("desktop-" + exp.date_started + index + Math.floor(Math.random() * Math.floor(1000))) } />);
+              for (let value of data_list[index].description) {
+                description_content.push(<span className="dark-mode-card-desc" key={ hash("desktop-" + exp.date_ended + index + Math.floor(Math.random() * Math.floor(1000)))}> { value } </span>);
+                description_content.push(<br key={ hash("desktop-" + exp.date_ended + index + Math.floor(Math.random() * Math.floor(1000))) } />);
+              }
+              description_content.push(<br key={ hash("desktop-" + exp.date_ended + index + Math.floor(Math.random() * Math.floor(1000))) } />);
 
+              return (
+                <div className="media mt-5" key={ hash("desktop-" + exp.date_ended + index + Math.floor(Math.random() * Math.floor(1000))) + exp.index }>
+                  <img src={ exp.img } className="mr-3 d-none d-lg-block" alt="..." />
+                  <img src={ exp.img } className="mr-3 d-lg-none" alt="..." height="45" width="45" />
+                  <div className="media-body border-bottom d-none d-lg-block">
+                    <h3 className="mt-0" style={{ fontSize: "1.2rem" }}>{ exp.title }</h3>
+                    <p style={{ fontFamily: "Titillium Web" }}>
+                        <span className="card-sub" style={{ fontWeight: "400" }}>{ exp.sub_title }</span>
+                        <br/>
+                        { date_element }
+                        <br/>
+                        <span className="card-location" style={{ color: props.dark_mode_props ? "rgba(226, 226, 226, 0.6)" : "rgba(0, 0, 0, 0.6)" }}> { exp.location } </span>
+                    </p>
+                    <span>
+                      { description_content }
+                    </span>
+                  </div>
+                  <div className="media-body border-bottom d-lg-none">
+                    <h3 className="mt-0 dark-mode-card-title" style={{ fontSize: "16px", fontWeight: "600" }}>{ exp.title }</h3>
+                    <p style={{ fontFamily: "Titillium Web" }}>
+                        <span className="card-sub" style={{ fontWeight: 400, fontSize: "14px" }}>{ exp.sub_title }</span>
+                        <br/>
+                        { date_element }
+                        <br/>
+                        <span className="card-location" style={{ fontSize: "13px", fontWeight: 500, color: props.dark_mode_props ? "rgba(226, 226, 226, 0.6)" : "rgba(0, 0, 0, 0.6)" }}> { exp.location } </span>
+                    </p>
+                    <span hidden={ data_list[index].description.length === 0 } style={{ fontSize: "12px" }}>
+                    <ExpandCollapse
+                      previewHeight="88px"
+                      expandText="more"
+                      collapse={false}
+                      >
+                        { description_content }
+                    </ExpandCollapse>
+
+
+                    </span>
+                  </div>
+                </div>
+              );
+
+            });
+
+            return (
+                data_cards
+            );
+      } else {
         return (
-          <div className="media mt-5" key={ hash("desktop-" + exp.date_ended + index + Math.floor(Math.random() * Math.floor(1000))) + exp.index }>
-            <img src={ exp.img } className="mr-3 d-none d-lg-block" alt="..." />
-            <img src={ exp.img } className="mr-3 d-lg-none" alt="..." height="45" width="45" />
-            <div className="media-body border-bottom d-none d-lg-block">
-              <h3 className="mt-0" style={{ fontSize: "1.2rem" }}>{ exp.title }</h3>
-              <p style={{ fontFamily: "Titillium Web" }}>
-                  <span className="card-sub" style={{ fontWeight: "400" }}>{ exp.sub_title }</span>
-                  <br/>
-                  { date_element }
-                  <br/>
-                  <span className="card-location" style={{ color: props.dark_mode_props ? "rgba(226, 226, 226, 0.6)" : "rgba(0, 0, 0, 0.6)" }}> { exp.location } </span>
-              </p>
-              <span>
-                { description_content }
-              </span>
-            </div>
-            <div className="media-body border-bottom d-lg-none">
-              <h3 className="mt-0 dark-mode-card-title" style={{ fontSize: "16px", fontWeight: "600" }}>{ exp.title }</h3>
-              <p style={{ fontFamily: "Titillium Web" }}>
-                  <span className="card-sub" style={{ fontWeight: 400, fontSize: "14px" }}>{ exp.sub_title }</span>
-                  <br/>
-                  { date_element }
-                  <br/>
-                  <span className="card-location" style={{ fontSize: "13px", fontWeight: 500, color: props.dark_mode_props ? "rgba(226, 226, 226, 0.6)" : "rgba(0, 0, 0, 0.6)" }}> { exp.location } </span>
-              </p>
-              <span hidden={ data_list[index].description.length === 0 } style={{ fontSize: "12px" }}>
-              <ExpandCollapse
-                previewHeight="88px"
-                expandText="more"
-                collapse={false}
-                >
-                  { description_content }
-              </ExpandCollapse>
-
-
-              </span>
-            </div>
-          </div>
+            <span></span>
         );
+      }
 
-      });
 
-      return (
-          data_cards
-      );
+
     }
 }
 
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -24,13 +24,13 @@ class Skills extends React.Component {
 
       return (
         <div style={{ width: "100%", margin: "0 auto" }}>
-                <Card data={ [{}] } show={false} />
+                <Card show={false} />
                 <div style={{width: '100%', height: "35px"}}></div>
                   <div className="card"  style={{width: '45%', minWidth: "300px", margin: "0 auto" }} id="card-content">
 
 
-                    <h2 style={{ textAlign: "center" }} className="mt-5">Skills (not finished)</h2>
-                    <p style={{ textAlign: "center" }} className="">(Summary)</p>
+                    <h2 style={{ textAlign: "center" }} className="mt-5">Skills</h2>
+                    <p style={{ textAlign: "center" }} className=""><Link to={"/about"} className="btn btn-mycolor"> About </Link></p>
 
                     <div className="card border-0 bg-transparent" style={{ width: '90%', margin: "0 auto"}}>
                       <div className="card-body">
@@ -45,75 +45,138 @@ class Skills extends React.Component {
                       </div>
                     </div>
 
-                    <hr style={{ width: "90%", margin: "0 auto" }}/>
+                    <hr style={{ width: "90%", margin: "0 auto", background: this.props.dark_mode_props ? "#383838" : "inherit" }}/>
+
+                    <h2 style={{ textAlign: "center" }} className="mt-5">Software proficiency</h2>
+
+                    <div className="card border-0 bg-transparent" style={{ width: '90%', margin: "0 auto"}}>
+                      <div className="card-body">
+                        <div className="software-icons">
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="3ds Max"
+                              className="software-icon-image"
+                              src="https://cdna.artstation.com/p/softwares/icons/000/000/002/default/3dsmax-20172.png?1504815710"
+                            />
+                            <div className="software-icon-name">3ds Max</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="Photoshop"
+                              className="software-icon-image"
+                              src="https://cdna.artstation.com/p/softwares/icons/000/000/032/default/photoshop.png?1567799381"
+                            />
+                            <div className="software-icon-name">Photoshop</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="ZBrush"
+                              className="software-icon-image"
+                              src="https://cdna.artstation.com/p/softwares/icons/000/000/046/default/zBrush3.png?1505488078"
+                            />
+                            <div className="software-icon-name">ZBrush</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="Fusion 360"
+                              className="software-icon-image"
+                              src="https://cdna.artstation.com/p/softwares/icons/000/000/016/default/fusion360.png?1504818877"
+                            />
+                            <div className="software-icon-name">Fusion 360</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="Substance Designer"
+                              className="software-icon-image"
+                              src="https://cdna.artstation.com/p/softwares/icons/000/000/042/default/Substance_Designer_Icon.png?1456833082"
+                            />
+                            <div className="software-icon-name">Substance Designer</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="Substance Painter"
+                              className="software-icon-image"
+                              src="https://cdnb.artstation.com/p/softwares/icons/000/000/041/default/Substance_Painter_Icon.png?1456833230"
+                            />
+                            <div className="software-icon-name">Substance Painter</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="3DCoat"
+                              className="software-icon-image"
+                              src="https://cdnb.artstation.com/p/softwares/icons/000/000/059/default/3d-coat_logo_ball.png?1511365214"
+                            />
+                            <div className="software-icon-name">3DCoat</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="Maya"
+                              className="software-icon-image"
+                              src="https://cdna.artstation.com/p/softwares/icons/000/000/024/default/maya.png?1504879343"
+                            />
+                            <div className="software-icon-name">Maya</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="UVLayout"
+                              className="software-icon-image"
+                              src="https://cdnb.artstation.com/p/softwares/icons/000/000/095/default/uvlayout.png?1424865993"
+                            />
+                            <div className="software-icon-name">UVLayout</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="Unreal Engine"
+                              className="software-icon-image"
+                              src="https://cdna.artstation.com/p/softwares/icons/000/000/044/default/unreal_logo_0.png?1471455994"
+                            />
+                            <div className="software-icon-name">Unreal Engine</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="Unity"
+                              className="software-icon-image"
+                              src="https://cdnb.artstation.com/p/softwares/icons/000/000/047/default/unity-logo-white.png?1479506284"
+                            />
+                            <div className="software-icon-name">Unity</div>
+                          </div>
+                          <div className="software-icon-item mr-3 ml-3">
+                            <img
+                              width={30}
+                              height={30}
+                              alt="CRYENGINE"
+                              className="software-icon-image"
+                              src="https://cdnb.artstation.com/p/softwares/icons/000/000/091/default/cryengine.png?1424866454"
+                            />
+                            <div className="software-icon-name">CRYENGINE</div>
+                          </div>
+                        </div>
+                        </div>
+                      </div>
+
                     <br/>
-                    <div className="card-deck" style={{ width: "90%", margin: "0 auto"}}>
-
-                      <div className="card text-white bg-dark border-0 mb-2" style={{  minWidth: "300px", maxWidth: "400px", width: "auto" }}>
-                        <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.spyrestudios.com%2Fwp-content%2Fuploads%2FReact.js_logo.svg_.png&f=1&nofb=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h3 className="card-title">React</h3>
-                        {/*  <p className="card-text">React (also known as React.js or ReactJS) is an open-source JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies.</p> */}
-                        </div>
-                        {/*
-                        <div className="card-footer">
-                          <small className="text-muted">https://reactjs.org/</small>
-                        </div>
-                        */}
-                      </div>
-
-                      <div className="card text-white bg-dark border-0 mb-2" style={{ minWidth: "300px", maxWidth: "400px", width: "auto"}}>
-                        <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.spyrestudios.com%2Fwp-content%2Fuploads%2FReact.js_logo.svg_.png&f=1&nofb=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h3 className="card-title">React Native</h3>
-                        {/*   <p className="card-text">React Native is an open-source mobile application framework created by Facebook, Inc</p> */}
-                        </div>
-                      </div>
-
-                      <div className="card text-white bg-dark border-0 mb-2" style={{ minWidth: "300px", maxWidth: "400px", width: "auto" }}>
-                        <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.spyrestudios.com%2Fwp-content%2Fuploads%2FReact.js_logo.svg_.png&f=1&nofb=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h3 className="card-title">react-router</h3>
-                        {/*   <p className="card-text">React Router is a collection of navigational components that compose declaratively with your application.</p> */}
-                        </div>
-                      </div>
-
-                      <div className="card text-white bg-dark border-0 mb-2" style={{ minWidth: "300px", maxWidth: "400px", width: "auto" }}>
-                        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fblog.couchbase.com%2Fwp-content%2Fuploads%2F2019%2F05%2FFlutter_Logo-1200x675.png&f=1&nofb=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h3 className="card-title">Flutter</h3>
-                        {/*   <p className="card-text">Flutter is an open-source UI software mobile development kit created by Google</p> */}
-                        </div>
-                      </div>
-
-                      <div className="card text-white bg-dark border-0 mb-2" style={{ minWidth: "300px", maxWidth: "400px", width: "auto" }}>
-                        <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.valuecoders.com%2Fblog%2Fwp-content%2Fuploads%2F2016%2F03%2FMeteor.png&f=1&nofb=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h3 className="card-title">Meteor.js</h3>
-                          {/* <p className="card-text">Meteor, or MeteorJS, is a free and open-source isomorphic JavaScript web framework written using Node.js</p> */}
-                        </div>
-                      </div>
-
-                      <div className="card text-white bg-dark border-0 mb-2" style={{ minWidth: "300px", maxWidth: "400px", width: "auto" }}>
-                        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F1%2F17%2FGraphQL_Logo.svg%2F1200px-GraphQL_Logo.svg.png&f=1&nofb=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h3 className="card-title">GraphQL</h3>
-                          {/* <p className="card-text">Meteor, or MeteorJS, is a free and open-source isomorphic JavaScript web framework written using Node.js</p> */}
-                        </div>
-                      </div>
-
-                      <div className="card text-white bg-dark border-0 mb-2" style={{ minWidth: "300px", maxWidth: "400px", width: "auto" }}>
-                        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F1%2F17%2FGraphQL_Logo.svg%2F1200px-GraphQL_Logo.svg.png&f=1&nofb=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h3 className="card-title">GraphQL</h3>
-                          {/* <p className="card-text">Meteor, or MeteorJS, is a free and open-source isomorphic JavaScript web framework written using Node.js</p> */}
-                        </div>
-                      </div>
-
-
-
-
-                    </div>
                     <br/>
                   </div>
 
